0cef2e212bd0dd38072fbdf484394760ea10b053
1E10 Laptop <sample@bths.edu>
2022-09-21 10:36:03 -0400

    initial files

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class HelloWorld {
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int a = 5;
+        double b = 8;
+        double c = (b/a);
+        System.out.println(c);
+
+    }
 }
